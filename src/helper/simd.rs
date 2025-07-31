32572e562909de136d9a6b6dc165d2fe0ccef5b3
ThisAccountHasBeenSuspended <176883156+ThisAccountHasBeenSuspended@users.noreply.github.com>
2025-07-31 04:20:14 +0200

    SIMD `NEON` improved
    Benchmarks showed an improvement.

--- src/helper/simd.rs
+++ src/helper/simd.rs
@@ -127,54 +127,40 @@ impl<'a, 'b> From<(&'a [u8], &'b mut usize)> for FindPositiveByteIndex<'a, 'b> {
 /// [Click here for more details about `_mm_movemask_epi8`](https://www.intel.com/content/www/us/en/docs/intrinsics-guide/index.html#text=_mm_movemask_epi8&ig_expand=4602)
 unsafe fn neon_movemask_epu8(value: arm::uint8x16_t) -> u16 {
     /*
-     * For more details and the "big endian" theory:
-     * https://github.com/utf-c/neon_movemask_epu8/blob/main/README.md
+     * Details:
+     * https://github.com/utf-c/neon_movemask_epu8/blob/v2/README.md
      */
     
     let (low, high): (u8, u8);
+    
     unsafe {
-        // We shift the bits of all elements (N=7) to the right.
-        let shift_u8 = arm::vshrq_n_u8(value, 7);
-        // We now turn the vector 16x-u8 into a 8x-u16.
-        let vec_8x16 = arm::vreinterpretq_u16_u8(shift_u8);
+        let msbs = arm::vshrq_n_u8(value, 7);
 
-        // We now pass the same vector twice to shift the bits of all elements
-        // in one of these vectors (N=7) to the right and accumulate them with the other.
-        let shift_u16 = arm::vsraq_n_u16(vec_8x16, vec_8x16, 7);
-        // We now turn the vector 8x-u16 into a 4x-u32.
-        let vec_4x32 = arm::vreinterpretq_u32_u16(shift_u16);
+        const WEIGHTS_ARRAY: [u8; 16] = [1, 2, 4, 8, 16, 32, 64, 128, 1, 2, 4, 8, 16, 32, 64, 128];
+        let weights = arm::vld1q_u8(WEIGHTS_ARRAY.as_ptr());
+        let weighted = arm::vmulq_u8(msbs, weights);
 
-        // We now pass the same vector twice to shift the bits of all elements
-        // in one of these vectors (N=14) to the right and accumulate them with the other.
-        let shift_u32 = arm::vsraq_n_u32(vec_4x32, vec_4x32, 14);
-        // We now turn the vector 4x-u32 into a 2x-u64.
-        let vec_2x64 = arm::vreinterpretq_u64_u32(shift_u32);
+        let sum16 = arm::vpaddlq_u8(weighted);
+        let sum32 = arm::vpaddlq_u16(sum16);
+        let sum64 = arm::vpaddlq_u32(sum32);
 
-        // We now pass the same vector twice to shift the bits of all elements
-        // in one of these vectors (N=28) to the right and accumulate them with the other.
-        let shift_u64 = arm::vsraq_n_u64(vec_2x64, vec_2x64, 28);
-        // Finally, we turn the vector 2x-u64 back into a 16x-u8.
-        let vec_16x8 = arm::vreinterpretq_u8_u64(shift_u64);
-
-        // Now we extract two elements (our "low" and "high") to get our result.
-        // NOTE: To understand why there are differences here, look at the theory.
         #[cfg(target_endian = "little")]
         {
             (low, high) = (
-                arm::vgetq_lane_u8(vec_16x8, 0),
-                arm::vgetq_lane_u8(vec_16x8, 8),
+                arm::vgetq_lane_u64(sum64, 0) as u8,
+                arm::vgetq_lane_u64(sum64, 1) as u8
             );
         }
         #[cfg(target_endian = "big")]
         {
-            // To get the correct result with "big endian", we have to reverse the bits of all elements.
-            let reversed = arm::vrbitq_u8(vec_16x8);
+            let sum_bytes = arm::vreinterpretq_u8_u64(sum64);
             (low, high) = (
-                arm::vgetq_lane_u8(reversed, 7),
-                arm::vgetq_lane_u8(reversed, 15),
+                arm::vgetq_lane_u64(sum_bytes, 7) as u8,
+                arm::vgetq_lane_u64(sum_bytes, 15) as u8
             );
         }
     } // unsafe
+
     ((high as u16) << 8) | (low as u16)
 }
 
